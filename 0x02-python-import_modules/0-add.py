b1827046339a9a7dbee5dba80e0804bdc83f554e
Felix Kipkorir <kipronofelix361@gmail.com>
2023-08-11 11:30:26 -0700

    0x02-python-import_modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,11 +2,8 @@
 
 if __name__ == "__main__":
 
-  import add_0 as total
+    import add_0 as total
 
-a = 1
-b = 2
-
-print(f"{'1 + 2'} = {total.add(a,b)}")
-
-
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, total.add(a, b)))
