c4796d71938ec3c55e58b835a6410557abab8f16
Felix Kipkorir <kipronofelix361@gmail.com>
2023-08-11 11:08:23 -0700

    0x02-python-import_modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 
-import add_0 as sum
+if __name__ == "__main__":
+
+  import add_0 as total
 
 a = 1
 b = 2
 
-print(f"{'1 + 2'} = {sum.add(a,b)}")
+print(f"{'1 + 2'} = {total.add(a,b)}")
+
+
