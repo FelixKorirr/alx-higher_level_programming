2964b1e54966e3514cb4fd02761c412b5f0e94ec
Felix Kipkorir <kipronofelix361@gmail.com>
2023-08-28 13:20:32 -0700

    0x05-python-exceptions

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 def safe_print_integer(value):
     try:
         int(value)
-        print("{}".format(value))
+        print(value)
         return True
     except ValueError:
         return False
