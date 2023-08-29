9c38f4a7dfa24ae9fae6b2979040d5f2ea8c43ba
Felix Kipkorir <kipronofelix361@gmail.com>
2023-08-29 03:54:35 -0700

    0x05-python-exceptions

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def safe_print_division(a, b):
 
     try:
@@ -8,5 +9,5 @@ def safe_print_division(a, b):
         total = None
 
     finally:
-        print("Inside result : {}".format(total))
+        print("Inside result: {}".format(total))
     return total
