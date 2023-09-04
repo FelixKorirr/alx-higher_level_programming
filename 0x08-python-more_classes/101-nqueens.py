36f73ff7868cd665ccfbe970a196df74b6961390
Felix Kipkorir <kipronofelix361@gmail.com>
2023-09-04 12:18:44 -0700

    0x08-python-more_classes

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -70,3 +70,18 @@ def x_out(x, y, z):
             break
         x[r][c] = "x"
         c -= 1
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    if sys.argv[1].isdigit() is False:
+        print("N must be a number")
+        sys.exit(1)
+    if int(sys.argv[1]) < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    x = _init_board(int(sys.argv[1]))
+    s = recursive_solve(x, 0, 0, [])
+    for i in s:
+        print(i)
