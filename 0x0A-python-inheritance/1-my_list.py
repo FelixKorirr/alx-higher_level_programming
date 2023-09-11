42d1bf94c5767db77418b70987015185441fd6b6
Felix Kipkorir <kipronofelix361@gmail.com>
2023-09-11 08:28:57 -0700

    0x0A-python-inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,6 +3,12 @@
 
 
 class MyList(list):
+    """Represent subclass"""
+
+    def __init__(self):
+        """Initialize MyList class""" 
+        pass
+
     def print_sorted(self):
         """This sorts a list in ascending order"""
         sorted_list = sorted(self)
