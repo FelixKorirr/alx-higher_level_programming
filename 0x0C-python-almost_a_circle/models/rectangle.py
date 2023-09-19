c476a62cdc9b820266d0f0b21c74a756186607c7
Felix Kipkorir <kipronofelix361@gmail.com>
2023-09-19 03:56:29 -0700

    models

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -27,10 +27,10 @@ class Rectangle(Base):
             raise ValueError("height must be > 0")
 
         if (x < 0):
-            raise ValueError("x must be > 0")
+            raise ValueError("x must be >= 0")
 
         if (y < 0):
-            raise ValueError("y must be > 0")
+            raise ValueError("y must be >= 0")
 
         self.__width = width
         self.__height = height
