baeda5748b7759d3b24310caf58716ccc01af63b
Felix Kipkorir <kipronofelix361@gmail.com>
2023-10-18 07:59:06 -0700

    0x0E-SQL_more_queries

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- List all privileges of MySql user_0d_1 on my server
+show grants for user_0d_1@localhost;
+
+-- List all privileges of MySql user_0d_2 on my server
+show grants for user_0d_2@localhost;
