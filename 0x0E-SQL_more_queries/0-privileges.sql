278df95c7b6dae87b578d562b42ab00b6544f08d
Felix Kipkorir <kipronofelix361@gmail.com>
2023-10-18 11:02:50 -0700

    0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,3 @@
--- List all privileges of MySql user_0d_1 on my server
+-- List all privileges of MySql user_0d_1 and user_0d_2  on my server
 show grants for user_0d_1@localhost;
-
--- List all privileges of MySql user_0d_2 on my server
 show grants for user_0d_2@localhost;
---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- Create the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on MySQL server
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- Use created database
+USE hbtn_0d_usa;
+-- Create a table
+CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
