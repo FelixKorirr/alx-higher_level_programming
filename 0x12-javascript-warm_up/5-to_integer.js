60de00682d2e102cc6c6e18bc57c1d726c9873a4
Felix Kipkorir <kipronofelix63@gmail.com>
2023-11-13 23:21:17 +0300

    JavaScript warm up

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-parsedValue = parseInt(process.argv[2])
+const parsedValue = parseInt(process.argv[2]);
 if (isNaN(parsedValue) || parsedValue === undefined) {
   console.log('Not a number');
 } else {
