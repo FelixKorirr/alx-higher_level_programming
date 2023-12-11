a5645eb6bf72ed93227c2c4f3dde38bf8c9b1bd3
Felix Kipkorir <kipronofelix63@gmail.com>
2023-12-11 21:04:52 +0300

    JAVASCRIPT WARM UP

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,19 +1,9 @@
 #!/usr/bin/node
-
-// let len = process.argv.length;
-
-// if (len <= 2) {
-//   console.log('0');
-// } else {
-//     for (let i = 0; i <= len; i++){
-//         if 
-//     }
-// } 
-
-let x = [1, 3, 2, 5, 4];
-sortedX = x.sort();
-
-function sortedValue() {
-    return sortedX[1]
-}
-sortedValue()
+if (process.argv.length <= 3) {
+    console.log('0');
+  } else {
+    const arr = process.argv.slice(2).map(Number);
+    const second = arr.sort(function (a, b) { return b - a; })[1];
+    console.log(second);
+  }
+  
