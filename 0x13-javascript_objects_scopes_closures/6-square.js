f7c31ee7fa52529c8c5da6dda6e7bef2e6b11329
Felix Kipkorir <kipronofelix63@gmail.com>
2023-12-12 17:30:54 +0300

    JAVASCRIPT OBJECTS

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const Square101 = require("./5-square");
+
+class Square extends Square101 {
+  constructor (size) {
+    super(size);
+  }
+
+  charPrint (c) {
+    for (let i = 0; i < this.size; i++) {
+      let row = '';
+      for (let j = 0; j < this.size; j++) {
+        if (c === undefined) {
+          row += 'X';
+        } else {
+          row += c;
+        }
+      }
+      console.log(row);
+    }
+  }
+}
+module.exports = Square2;
